ddfd178899c8963216fa5e32aa6958d4fe1a4bc6
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2022-07-07 11:28:24 -0300

    Added table view

--- AppViagens/AppViagens/ViewController.swift
+++ AppViagens/AppViagens/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tripsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tripsTableView.dataSource = self
     }
+    
+}
 
-
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = "viagem \(indexPath.row)"
+        return cell
+    }
 }
 
