6cd93e09ddbc948fdcf5adce1aec72a4e3766ec7
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2022-07-10 23:41:22 -0300

    added iboutlets

--- AppViagens/AppViagens/Views/Celulas/Oferta/OfertaTableViewCell.swift
+++ AppViagens/AppViagens/Views/Celulas/Oferta/OfertaTableViewCell.swift
@@ -9,5 +9,12 @@ import UIKit
 
 class OfertaTableViewCell: UITableViewCell {
     
+//MARK: -IBOutlets
+    
+    @IBOutlet var viagemImages: [UIImageView]!
+    @IBOutlet var tituloViagemLabels: [UILabel]!
+    @IBOutlet var subtituloViagemLabels: [UILabel]!
+    @IBOutlet var precoSemDescontoLabels: [UILabel]!
+    @IBOutlet var precoLabels: [UILabel]!
     
 }
